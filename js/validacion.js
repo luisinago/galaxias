0a64400b2f77f47103b03b51d98551fb855c5ac1
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2024-08-22 19:32:41 -0300

    funcioncampos

--- js/validacion.js
+++ js/validacion.js
@@ -6,3 +6,16 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
+function controlCampos(){
+
+    let nom = document.getElementById('nombre').value;
+    let ape = document.getElementById('apellido').value;
+    let email = document.getElementById('email').value;
+    let pass1 = document.getElementById('password1').value;
+    let pass2 = document.getElementById('password2').value;
+    
+    if(nom == ""||ape== ""||email==""||pass1==""||pass2 ==""){
+        showAlertError();
+        return;
+    }
+}
